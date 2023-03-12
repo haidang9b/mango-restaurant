b2b9729403897faaf63094b692e42f1f85b11443
Phan Hải Đăng <drom97977@gmail.com>
2023-03-12 15:56:27 +0700

    Entity model & dto for shopping cart

---
+++ Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey(nameof(CartHeaderId))]
+        public virtual CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+        public int Count { get; set; }
+    }
+}
