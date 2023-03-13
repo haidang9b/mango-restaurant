50c36186cf9b61e40af126f270564b2530e1554c
Phan Hải Đăng <drom97977@gmail.com>
2023-03-13 15:45:02 +0700

    Implement method load shopping cart

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+        public async Task<IActionResult> Index()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(item => item.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.GetCartByUserIdAsync<ResponseDto>(userId, accessToken);
+            CartDto cartDto = new();
+            if(response != null && response.IsSuccess)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
+            }
+            if(cartDto.CartHeader != null)
+            {
+                foreach(var detail in cartDto.CartDetails )
+                {
+                    cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+                }
+            }
+            return cartDto;
+        }
+    }
+}
